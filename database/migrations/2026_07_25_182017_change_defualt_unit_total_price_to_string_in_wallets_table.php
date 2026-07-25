68063ce7dc6bfb591f8518308a9e2ea5baae18c5
NancyAhmed97 <anancyahmed97@icloud.com>
2026-07-25 21:21:09 +0300

    sone

---
+++ database/migrations/2026_07_25_182017_change_defualt_unit_total_price_to_string_in_wallets_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->string('defualt_unit_total_price', 50)->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->decimal('defualt_unit_total_price', 10, 2)->nullable()->change();
+        });
+    }
+};
